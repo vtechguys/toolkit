f4de41e51bc8565a550808b8d060dc87210867d7
vtechguys <aniketjha898@gmail.com>
2024-04-11 18:57:44 +0530

    refactor(files): commonised some files

--- src/app/bg-remove/components.tsx
+++ src/app/bg-remove/components.tsx
@@ -1,4 +1,84 @@
+import React from "react";
+import { ImagePreview } from "./components";
 import { AsyncStatus } from "./types";
+
+export interface ImageUploaderProps {
+  imageBase64: string | null;
+  status: AsyncStatus;
+  onUploadSuccess: (name: string, fileBase64: string) => void;
+  onUploadStart: () => void;
+}
+
+export function ImageUploader(props: ImageUploaderProps) {
+  const { imageBase64, status, onUploadSuccess, onUploadStart } = props;
+
+  const readFile = (file: File) => {
+    onUploadStart();
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      const base64String = reader.result as string;
+      onUploadSuccess(file.name, base64String);
+    };
+  };
+
+  const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      readFile(file);
+    }
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files?.[0];
+    if (file) {
+      readFile(file);
+    }
+  };
+
+  const handlePaste = (e: React.ClipboardEvent<HTMLDivElement>) => {
+    const items = e.clipboardData.items;
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].kind === "file" && items[i].type.match("^image/")) {
+        const file = items[i].getAsFile();
+        if (file) {
+          readFile(file);
+        }
+        break;
+      }
+    }
+  };
+
+  return status === "idle" ? (
+    <div
+      className="w-full flex flex-row grow-0 basis-full border border-dashed border-gray-400 rounded-lg relative overflow-hidden"
+      onDrop={handleDrop}
+      onDragOver={(e) => e.preventDefault()}
+      onPaste={handlePaste}
+    >
+      <div className="absolute inset-0 flex items-center justify-center text-gray-400">
+        Drop Image Here or &nbsp;<span className="underline">Browse</span>
+        <input
+          type="file"
+          accept="image/*"
+          className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+          onChange={handleFileInputChange}
+        />
+      </div>
+    </div>
+  ) : (
+    <ImagePreview
+      src={imageBase64}
+      minimize={true}
+      alt="Uploaded"
+      status={status}
+    />
+  );
+}
 
 interface ImagePreviewProps {
   src: string | null;
--- src/app/bg-remove/tool.tsx
+++ src/app/bg-remove/tool.tsx
@@ -1,9 +1,9 @@
 "use client";
 import dynamic from "next/dynamic";
 import { useEffect, useRef, useState } from "react";
-import { ImageUploader, ImageUploaderProps } from "./ImageUploader";
+import { ImageUploader, ImageUploaderProps } from "./components";
 import { IConfig, getConfig } from "./config";
-import { preload } from "./preload";
+import type { Config } from "@imgly/background-removal";
 import { AsyncStatus } from "./types";
 
 const Output = dynamic(() => import("./BgRemover"));
@@ -19,7 +19,69 @@ type OutputImageState = Omit<ImageArea, "name"> & {
   phase: "assets" | "process";
 };
 
-const getUserMessage = (percent: number, phase: "assets" | "process") => {
+async function preload(config: Config) {
+  const resourceUrl = new URL("resources.json", config.publicPath);
+  const resourceResponse = await fetch(resourceUrl);
+  if (!resourceResponse.ok) {
+    throw new Error(
+      `Resource metadata not found. Ensure that the config.publicPath is configured correct l: ${config.publicPath}`
+    );
+  }
+  const resourceMap = await resourceResponse.json();
+  const keys = Object.keys(resourceMap);
+  await Promise.all(
+    keys.map(async (key) => {
+      return loadAsBlob(key, config);
+    })
+  );
+}
+
+async function loadAsBlob(key: string, config: Config) {
+  const resourceUrl = new URL("resources.json", config.publicPath);
+  const resourceResponse = await fetch(resourceUrl);
+  if (!resourceResponse.ok) {
+    throw new Error(
+      `Resource metadata not found. Ensure that the config.publicPath is configured correctly.`
+    );
+  }
+  const resourceMap = await resourceResponse.json();
+  const entry = resourceMap[key];
+  if (!entry) {
+    throw new Error(
+      `Resource ${key} not found. Ensure that the config.publicPath is configured correctly.`
+    );
+  }
+  const chunks = entry.chunks;
+  let downloadedSize = 0;
+  const responses = chunks.map(async (chunk: any) => {
+    const chunkSize = chunk.offsets[1] - chunk.offsets[0];
+    const url = config.publicPath
+      ? new URL(chunk.hash, config.publicPath).toString()
+      : chunk.hash;
+    const response = await fetch(url, config.fetchArgs);
+    const blob = await response.blob();
+    if (chunkSize !== blob.size) {
+      throw new Error(
+        `Failed to fetch ${key} with size ${chunkSize} but got ${blob.size}`
+      );
+    }
+    if (config.progress) {
+      downloadedSize += chunkSize;
+      config.progress(`fetch:${key}`, downloadedSize, entry.size);
+    }
+    return blob;
+  });
+  const allChunkData = await Promise.all(responses);
+  const data = new Blob(allChunkData, { type: entry.mime });
+  if (data.size !== entry.size) {
+    throw new Error(
+      `Failed to fetch ${key} with size ${entry.size} but got ${data.size}`
+    );
+  }
+  return data;
+}
+
+function getUserMessage(percent: number, phase: "assets" | "process") {
   let message = "";
 
   if (phase === "assets") {
@@ -39,7 +101,7 @@ const getUserMessage = (percent: number, phase: "assets" | "process") => {
   }
 
   return message;
-};
+}
 
 export function Tool() {
   const timeoutRef = useRef<NodeJS.Timeout>();
